0337ae77d58b6f921d00e032bafa813721647fe1
Sergey White <sergey_white86@mail.ru>
2024-11-04 17:52:06 +0300

    init commit

---
+++ src/test/java/org/sky_pro/team_work/TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sky_pro.team_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
