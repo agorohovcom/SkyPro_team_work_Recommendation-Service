7a9aed81581587702c9269e1526bc793313f7d54
agorohov <aleksandr.gorohov@gmail.com>
2024-11-11 23:18:02 +0300

    добавил к ендпоинту @Operation, @ApiResponses и @Parameter для вывода в Swagger

--- src/main/java/org/sky_pro/team_work/controller/RecommendationsController.java
+++ src/main/java/org/sky_pro/team_work/controller/RecommendationsController.java
@@ -1,5 +1,11 @@
 package org.sky_pro.team_work.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.sky_pro.team_work.dto.RecommendationDto;
 import org.sky_pro.team_work.service.RecommendationsService;
@@ -19,8 +25,19 @@ public class RecommendationsController {
 
     private final RecommendationsService service;
 
+    @Operation(summary = "Get personal recommendations for user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the recommendations",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecommendationDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content(mediaType = "application/json"))})
     @GetMapping("/{user_Id}")
-    public ResponseEntity<RecommendationDto> getPersonalRecommendations(@PathVariable UUID user_Id) {
+    public ResponseEntity<RecommendationDto> getPersonalRecommendations(
+            @Parameter(description = "User id to get personal recommendations")
+            @PathVariable UUID user_Id) {
 
         RecommendationDto recommendations = new RecommendationDto();
         recommendations.setUserId(user_Id);
