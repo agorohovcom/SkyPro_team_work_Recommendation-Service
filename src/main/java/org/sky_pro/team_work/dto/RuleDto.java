87012f3b0f18c5068539915e3941b43499acb708
agorohov <aleksandr.gorohov@gmail.com>
2024-11-29 13:16:19 +0300

    Добавил валидацию полей RuleDto при добавлении нового правила через addRule

--- src/main/java/org/sky_pro/team_work/dto/RuleDto.java
+++ src/main/java/org/sky_pro/team_work/dto/RuleDto.java
@@ -1,6 +1,7 @@
 package org.sky_pro.team_work.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,13 @@ import java.util.List;
 @ToString
 public class RuleDto {
     @JsonProperty("product_name")
+    @NotBlank(message = "Имя продукта не может быть пустым")
     private String productName;
     @JsonProperty("product_id")
+    @NotBlank(message = "Id продукта не может быть пустым")
     private String productId;
     @JsonProperty("product_text")
+    @NotBlank(message = "Текст продукта не может быть пустым")
     private String productText;
     @JsonProperty("rule")
     private List<Query> query;
--- src/main/java/org/sky_pro/team_work/exception/RecommendationServiceExceptionHandler.java
+++ src/main/java/org/sky_pro/team_work/exception/RecommendationServiceExceptionHandler.java
@@ -3,6 +3,7 @@ package org.sky_pro.team_work.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -38,6 +39,16 @@ public class RecommendationServiceExceptionHandler {
             errorResponse.put("message", ex.getMessage());
             errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
         }
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errorResponse.put(error.getField(), error.getDefaultMessage()));
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
