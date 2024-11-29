43c98c6dd027ab727531f9aa966859d13d589d76
agorohov <aleksandr.gorohov@gmail.com>
2024-11-29 14:30:18 +0300

    Добавил handleHttpMessageNotReadableException и handleMethodArgumentNotValidException

--- src/main/java/org/sky_pro/team_work/domain/Query.java
+++ src/main/java/org/sky_pro/team_work/domain/Query.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.sky_pro.team_work.enums.QueryType;
 
@@ -19,11 +21,14 @@ public class Query {
 
  @JsonProperty("query")
  @Enumerated(EnumType.STRING)
+ @NotNull(message = "Значение не может отсутствовать")
  private QueryType queryType;
 
  @JsonProperty("arguments")
+ @NotEmpty(message = "Список аргументов не может быть пустым")
  private List<String> arguments;
 
  @JsonProperty("negate")
+ @NotNull(message = "Значение не может отсутствовать")
  private Boolean negate;
 }
--- src/main/java/org/sky_pro/team_work/exception/RecommendationServiceExceptionHandler.java
+++ src/main/java/org/sky_pro/team_work/exception/RecommendationServiceExceptionHandler.java
@@ -1,8 +1,12 @@
 package org.sky_pro.team_work.exception;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +15,7 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -18,27 +23,27 @@ public class RecommendationServiceExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException re) {
-        log.error(re.getMessage());
+        String message = re.getMessage();
         Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("message", re.getMessage());
+        errorResponse.put("message", message);
         errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        log.error(message);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Map<String, Object>> handleMethodArgumentTypeMismatchException(
             MethodArgumentTypeMismatchException ex) {
+        String message;
         Map<String, Object> errorResponse = new HashMap<>();
         if (ex.getRequiredType() != null && ex.getRequiredType().equals(UUID.class)) {
-            String message = "Полученный id не является UUID";
-            log.error(message);
-            errorResponse.put("message", message);
-            errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+            message = "Полученный id не является UUID";
         } else {
-            log.error(ex.getMessage());
-            errorResponse.put("message", ex.getMessage());
-            errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+            message = "Ошибка: " + ex.getMessage();
         }
+        errorResponse.put("message", message);
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        log.error(message);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -49,6 +54,34 @@ public class RecommendationServiceExceptionHandler {
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errorResponse.put(error.getField(), error.getDefaultMessage()));
         errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        log.error("Ошибка валидации: {}", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex) {
+        String message;
+        Map<String, Object> errorResponse = new HashMap<>();
+        if (ex.getCause() instanceof InvalidFormatException ife) {
+            String fieldName = ife.getPath().getFirst().getFieldName();
+            message = "Некорректное значение в поле '" + fieldName + "': " + ife.getValue();
+            errorResponse.put("message", message);
+        } else if (ex.getCause() instanceof JsonParseException jpe) {
+            message = "Ошибка парсинга JSON: " + jpe.getOriginalMessage();
+            errorResponse.put("message", message);
+        } else if (ex.getCause() instanceof JsonMappingException jme) {
+            String fieldName = jme.getPath().stream()
+                    .map(JsonMappingException.Reference::getFieldName)
+                    .collect(Collectors.joining("."));
+            message = "Ошибка маппинга JSON: " + jme.getOriginalMessage() + " в поле '" + fieldName + "'";
+            errorResponse.put("message", message);
+        } else {
+            message = "Ошибка чтения сообщения: " + ex.getMessage();
+            errorResponse.put("message", message);
+        }
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        log.error(message);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
